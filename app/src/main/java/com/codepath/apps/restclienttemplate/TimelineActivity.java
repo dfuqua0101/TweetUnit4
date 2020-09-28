f7b7964c300aaaf98131538f33d67b641fdbde89
dfuqua0101 <69980379+dfuqua0101@users.noreply.github.com>
2020-09-27 23:15:05 -0500

    Changed some stuff

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -14,6 +15,9 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
+import com.codepath.apps.restclienttemplate.models.TweetDao;
+import com.codepath.apps.restclienttemplate.models.TweetWithUser;
+import com.codepath.apps.restclienttemplate.models.User;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 
 import org.json.JSONArray;
@@ -30,6 +34,7 @@ public class TimelineActivity extends AppCompatActivity {
     public static final String TAG = "TimelineActivity";
     private final int REQUEST_CODE = 20;
 
+    TweetDao tweetDao;
     TwitterClient client;
     RecyclerView rvTweets;
     List<Tweet> tweets;
@@ -43,6 +48,7 @@ public class TimelineActivity extends AppCompatActivity {
         setContentView(R.layout.activity_timeline);
 
         client = TwitterApp.getRestClient( this);
+        final TweetDao tweetDao = ((TwitterApp) getApplicationContext()).getMyDatabase().tweetDao();
 
         swipeContainer = findViewById(R.id.swipeContainer);
         //Configure the refreshing colors
@@ -77,6 +83,18 @@ public class TimelineActivity extends AppCompatActivity {
         };
         //Adds the scroll listener to RecyclerView
         rvTweets.addOnScrollListener(scrollListener);
+
+        //Query for existing tweets in the DB
+        AsyncTask.execute(new Runnable() {
+           @Override
+           public void run() {
+               Log.i(TAG,"Showing data from database");
+               List<TweetWithUser> tweetWithUsers = tweetDao.recentItems();
+               List<Tweet> tweetsFromDB = TweetWithUser.getTweetList(tweetWithUsers);
+               adapter.clear();
+               adapter.addAll(tweetsFromDB);
+           }
+        });
 
         populateHomeTimeline();
     }
@@ -125,7 +143,7 @@ public class TimelineActivity extends AppCompatActivity {
                 //2. Deserialize and construct new model objects from the API response
                 JSONArray jsonArray = json.jsonArray;
                 try {
-                    List<Tweet> tweets = Tweet.fromjsonArray(jsonArray);
+                    List<Tweet> tweets = Tweet.fromJsonArray(jsonArray);
                     //3. Append the new data objects to the existing set of items inside the array of items
                     //4. Notify the adapter of the new items made with 'notifyItemRangeInserted()'
                     adapter.addAll(tweets);
@@ -147,14 +165,27 @@ public class TimelineActivity extends AppCompatActivity {
             public void onSuccess(int statusCode, Headers headers, JSON json) {
                 Log.i(TAG, "onSuccess! " + json.toString());
                 JSONArray jsonArray = json.jsonArray;
-                adapter.clear();
                 try {
-                    adapter.addAll(Tweet.fromjsonArray(jsonArray));
+                    final List<Tweet> tweetsFromNetwork = Tweet.fromJsonArray(jsonArray);
+                    adapter.clear();
+                    adapter.addAll(tweetsFromNetwork);
+                    //Now we call setRefreshing(false) to signal refresh has finished
+                    swipeContainer.setRefreshing(false);
+                    AsyncTask.execute(new Runnable(){
+                        @Override
+                        public void run() {
+                            Log.i(TAG,"Saving data into database");
+
+                            //insert users first
+                            List<User> usersFromNetwork = User.fromJsonTweetArray(tweetsFromNetwork);
+                            tweetDao.insertModel(usersFromNetwork.toArray(new User[0]));
+                            //insert tweets next
+                            tweetDao.insertModel(tweetsFromNetwork.toArray(new Tweet[0]));
+                        }
+                    });
                 } catch (JSONException e) {
-                    e.printStackTrace();
+                    Log.e(TAG,"Json exception", e);
                 }
-                //Now we call setRefreshing(false) to signal refresh has finished
-                swipeContainer.setRefreshing(false);
             }
 
             @Override
--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -1,5 +1,13 @@
 package com.codepath.apps.restclienttemplate.models;
 
+import android.provider.ContactsContract;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -9,11 +17,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Parcel
+@Entity(foreignKeys = @ForeignKey(entity=User.class, parentColumns="id", childColumns="userId"))
 public class Tweet {
 
-    public String body;
-    public String createdAt;
+    @PrimaryKey
+    @ColumnInfo
     public long id;
+
+    @ColumnInfo
+    public String body;
+
+    @ColumnInfo
+    public String createdAt;
+
+    @ColumnInfo
+    public long userId;
+
+    @Ignore
     public User user;
 
     // empty constructor needed by the Parceler library
@@ -24,19 +44,17 @@ public class Tweet {
         tweet.body = jsonObject.getString( "text");
         tweet.createdAt = jsonObject.getString( "created_at");
         tweet.id = jsonObject.getLong( "id");
-        tweet.user = User.fromJson(jsonObject.getJSONObject("user"));
+        User user = User.fromJson(jsonObject.getJSONObject("user"));
+        tweet.user = user;
+        tweet.userId = user.id;
         return tweet;
     }
 
-    public static List<Tweet> fromjsonArray(JSONArray jsonArray) throws JSONException {
+    public static List<Tweet> fromJsonArray(JSONArray jsonArray) throws JSONException {
         List<Tweet> tweets = new ArrayList<>();
         for (int i = 0; i < jsonArray.length(); i++) {
             tweets.add(fromJson(jsonArray.getJSONObject(i)));
         }
         return tweets;
-    }
-
-    public static List<Tweet> fromJsonArray(JSONArray jsonArray) {
-        return null;
     }
 }
